f87e58a2b42b4bce81971e8b2ded308fa4d5db82
Sébastien GAYA <somniia.99@gmail.com>
2018-01-06 04:31:52 +0100

    Extension page lists uptime achievements !

--- extpage/extpage.js
+++ extpage/extpage.js
@@ -56,12 +56,63 @@ function generateOnetimeElt(achievement, isLast = false) {
       onetimeElt = onetimeElt.replace(/__achieved-subtitle__/, "");
     } else {
       onetimeElt = onetimeElt.replace(/__achieved__/, "achieved");
-      var formattedDate = result[achievement["id"]]["date"].toLocaleString()
+      var formattedDate = result[achievement["id"]]["date"]
       onetimeElt = onetimeElt.replace(/__achieved-subtitle__/, " <em>(obtenu le "+formattedDate+")</em>");
     }
     onetimeAchievements += onetimeElt;
     if (isLast) {
       onetimeAchievementsElt.innerHTML = onetimeAchievements;
+    }
+  });
+}
+
+var uptimeAchievementsElt = document.querySelector("#uptime-achievements");
+var uptimeAchievements = "";
+let uptimeProto = `<div class="uptime-achievement __achieved__">
+  <div class="icon-container">
+    <img class="level-__level__" src="__img-link__" alt="__name__">
+  </div>
+  <div class="text-container">
+    <p><strong>__name____achieved-subtitle__</strong></p>
+    <p>__description__</p>
+  </div>
+</div>`;
+
+getJSON("../achievements/uptime.json").then(function(json) {
+  for (var i = 0 ; i < json.length ; i++) {
+    if (i == json.length - 1) {
+      generateUptimeElt(json[i], true);
+    } else {
+      generateUptimeElt(json[i]);
+    }
+  }
+});
+
+function generateUptimeElt(achievement, isLast = false) {
+  var uptimeElt = uptimeProto;
+  uptimeElt = uptimeElt.replace(/__name__/g, achievement["name"]);
+  chrome.storage.sync.get(achievement["id"], function(result) {
+    let storageData = result[achievement["id"]];
+    let descriptionIndex = Math.max(1, storageData["level"]) - 1;
+    uptimeElt = uptimeElt.replace(/__description__/, achievement["descriptionByLevel"][descriptionIndex]);
+
+    if(achievement["icon"] !== undefined) {
+      uptimeElt = uptimeElt.replace(/__img-link__/, "../"+achievement["icon"]);
+    } else {
+      uptimeElt = uptimeElt.replace(/__img-link__/, "../logo.png");
+    }
+    if(storageData["achievedDate"] === undefined) {
+      uptimeElt = uptimeElt.replace(/__achieved__/, "");
+      uptimeElt = uptimeElt.replace(/__achieved-subtitle__/, "");
+    } else {
+      uptimeElt = uptimeElt.replace(/__achieved__/, "achieved");
+      var formattedDate = storageData["achievedDate"]
+      uptimeElt = uptimeElt.replace(/__achieved-subtitle__/, " <em>(niveau "+storageData["level"]+" obtenu le "+formattedDate+")</em>");
+    }
+
+    uptimeAchievements += uptimeElt;
+    if (isLast) {
+      uptimeAchievementsElt.innerHTML = uptimeAchievements;
     }
   });
 }
